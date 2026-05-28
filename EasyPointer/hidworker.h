624bbdd75423b97c2efad60ed6a0f82b9d8f4a0b
叶公好龙 <wlffeng2008@163.com>
2026-05-28 12:28:57 +0800

    TX ASR done!

--- EasyPointer/hidworker.h
+++ EasyPointer/hidworker.h
@@ -21,8 +21,109 @@
 #include <QFile>
 #include <QIODevice>
 
+
+#include <QJsonDocument>
+#include <QJsonObject>
+
+
+#include <QWebSocket>
+#include <QAudioInput>
+#include <QAudioSource>
+#include <QCryptographicHash>
+#include <QMessageAuthenticationCode>
+#include <QUrl>
+#include <QDateTime>
+#include <QUuid>
+#include <QRandomGenerator>
+
+
+class AsrClient : public QObject
+{
+    Q_OBJECT
+public:
+    AsrClient(const QUrl& url, QObject* parent = nullptr)
+        : QObject(parent)
+    {
+        m_ws = new QWebSocket();
+        connect(m_ws, &QWebSocket::connected, this, &AsrClient::onConnected);
+        connect(m_ws, &QWebSocket::textMessageReceived, this, &AsrClient::onTextMsg);
+        connect(m_ws, QOverload<QAbstractSocket::SocketError>::of(&QWebSocket::error),
+                [](QAbstractSocket::SocketError e){ qDebug()<<"WS Error:"<<e; });
+
+        m_url = url ;
+        start();
+    }
+    ~AsrClient(){}
+
+    void stop()
+    {
+        if (m_ws->state() == QAbstractSocket::ConnectedState)
+            m_ws->sendTextMessage("{\"type\":\"end\"}");
+        m_audioDevice->close();
+        m_audioSource->stop();
+        m_ws->close();
+    }
+
+    void start(){ m_ws->open(m_url); }
+
+private slots:
+    void onConnected()
+    {
+        qDebug() << "ASR WebSocket Connected!";
+        startCapture();
+    }
+
+    void onTextMsg(const QString& msg)
+    {
+        // {"code":0,"result":{"slice_type":2,"voice_text_str":"你好"},...}
+        QJsonDocument jDoc = QJsonDocument::fromJson(msg.toUtf8());
+        QJsonObject jObj = jDoc.object();
+        if (jObj["code"].toInt() == 0)
+        {
+            QJsonObject r = jObj["result"].toObject();
+            if (r["slice_type"].toInt() == 2)  // 句子结束
+                qDebug() << "识别结果:" << r["voice_text_str"].toString();
+        }
+        else
+        {
+            qWarning().noquote() << "ASR Error:" << msg;
+        }
+    }
+
+    void startCapture()
+    {
+        QAudioDevice inputDevice = QMediaDevices::defaultAudioInput();
+        QAudioFormat fmt = inputDevice.preferredFormat();
+        fmt.setSampleRate(16000);
+        fmt.setChannelCount(1);
+        fmt.setSampleFormat(QAudioFormat::Int16);
+
+        m_audioSource = new QAudioSource(inputDevice,fmt);
+        // 建议每 40ms 读一次 → 16000 * 2 * 0.04 = 1280 bytes
+        m_audioDevice = m_audioSource->start();
+        m_audioSource->setBufferSize(1280);
+        connect(m_audioDevice, &QIODevice::readyRead, this, [this]() {
+            while (m_audioDevice->bytesAvailable() >= 1280)
+            {
+                QByteArray pcm = m_audioDevice->read(1280);
+                if (!pcm.isEmpty() && m_ws->state() == QAbstractSocket::ConnectedState)
+                    m_ws->sendBinaryMessage(pcm);
+            }
+        });
+    }
+
+private:
+    QUrl  m_url ;
+    QWebSocket   *m_ws = nullptr;
+    QIODevice    *m_audioDevice = nullptr;
+    QAudioSource *m_audioSource = nullptr;
+};
+
+
 bool PCMFile2WAVFile(const QString&strPCMFile,const QString&strWAVFile);
 bool WAVFile2MP3File(const QString&strWAVFile,const QString&strMP3File);
+
+void DoASRWork(bool toStart=true);
 
 class CHidWorker : public QThread
 {
